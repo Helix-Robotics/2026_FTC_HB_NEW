d623821ad3c4b1b092dbc546d94ac9f61fad2919
Leaferd6712 <157940915+Leaferd6712@users.noreply.github.com>
2026-09-25 09:38:58 +1000

    pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/ShooterV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/ShooterV2.java
@@ -15,7 +15,7 @@ public class ShooterV2 extends Shooter {
     public static double V2_SHOOTER_P = 23;
     public static double V2_SHOOTER_I = 0;
     public static double V2_SHOOTER_D = 1.25;
-    public static double V2_SHOOTER_F = 15.4;
+    public static double V2_SHOOTER_F = 15.25;
     public static double V2_TARGET_VELOCITY = -1140;
     public static double V2_MIN_VELOCITY = -1120;
 
