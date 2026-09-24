f6d8120fa847d9ded8807aea624c0c606660ac52
Leaferd6712 <157940915+Leaferd6712@users.noreply.github.com>
2026-09-24 13:37:20 +1000

    main v2 red

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/MainV2Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/MainV2Red.java
@@ -3,12 +3,10 @@ package org.firstinspires.ftc.teamcode.teleops;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.commands.CommandAbstract;
 import org.firstinspires.ftc.teamcode.commands.CommandsV1;
-import org.firstinspires.ftc.teamcode.robot.subsystems.Feeder;
 
-@TeleOp (name = "Main v2")
-public class MainV2 extends MainV1Red {
+@TeleOp (name = "MainV2Red")
+public class MainV2Red extends MainV1Red {
     @Override
     public void init() {
         // tune inPerTick for ur drivetrain encoders
